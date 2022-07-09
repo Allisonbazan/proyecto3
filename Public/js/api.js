d6470ec0ec053969133e6f43e0af5b2f4ceb1721
hterreros <harold.terreros.talledo@gmail.com>
2022-07-09 14:02:52 -0500

    documentacion en api.js

--- Public/js/api.js
+++ Public/js/api.js
@@ -4,12 +4,14 @@ const APIDigimon = 'https://digimon-api.vercel.app/api/digimon/';
 
 export const getDigimon = async () => {
     try{
+        //consumo de la api
         const getDigimonApi = await fetch(APIDigimon)
         const data = await getDigimonApi.json();
-
+        //construcción de combo
         for (const property in data) {
             VarOpt = VarOpt+`<option value="${property}">${data[property].name}</option>`
         }
+        //Datos del digimon inicial que se muestra en pantalla
         document.getElementById("digimonName").innerHTML = data[`${ID}`].name
         document.getElementById("digimonLevel").innerHTML = '<h6><strong> LEVEL: </strong> </h6>'+data[`${ID}`].level
         document.getElementById("digimonImage").innerHTML = `<img src="${data[`${ID}`].img}" class="card-img-top" alt="">`
@@ -18,6 +20,7 @@ export const getDigimon = async () => {
         return data  
 
     }catch{
+        //si existe un problema con la conexión al api
         console.log("Hay error en la llamada al api")
     }
 }
